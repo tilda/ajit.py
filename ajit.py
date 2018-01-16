088ac02b6f45fabe67212ba4625df5301a416c60
S Stewart <tilda@users.noreply.github.com>
2018-01-15 22:44:23 -0600

    first and last commit I will do

---
+++ ajit.py
@@ -0,0 +1,21 @@
+class Ajit:
+    def __init__(self):
+        pass
+
+    def post_fake_comments(self):
+        """Post fake FCC comments."""
+        print("Stealing identities.....")
+        print("done.")
+        print("Connecting to servers.....")
+        print("done.")
+        print("Sending API requests.....")
+        print("done.")
+    
+    def vote(self):
+        """Totally fair voting."""
+        print("We have come to a conclusion to remove Net Neutrality.")
+
+    def destroy_internet(self):
+        """Destroys internet. Main function."""
+        self.post_fake_comments()
+        self.vote()
